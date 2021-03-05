00df838703d27ec27c4232813fc80dc6bee6b217
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-03-05 17:48:27 -0300

    Fixação do valor randômico

--- Recomendador/src/main/java/br/maua/Avaliador.java
+++ Recomendador/src/main/java/br/maua/Avaliador.java
@@ -6,6 +6,7 @@ import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
 import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.common.RandomUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,14 +15,18 @@ public class Avaliador {
 
     public static void main(String[] args) throws IOException, TasteException {
 
+        // Fixa o valor randômico da análise
+        RandomUtils.useTestSeed();  // Impede que a média mude. Será sempre o mesmo valor
+
         File file = new File("dados.csv");
         // Modelo de Análise
         DataModel model = new FileDataModel(file);
 
-       RecommenderEvaluator evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
+        RecommenderEvaluator evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
+        // Construtor de Recomendações
         RecommenderBuilder builder = new RecomendadorDeProdutosBuilder();
-       // 90% para treinar e 10% para testar
-       double error = evaluator.evaluate(builder, null, model, 0.9, 1.0);
+        // 90% para treinar e 10% para testar
+        double error = evaluator.evaluate(builder, null, model, 0.9, 1.0);
 
         System.out.println(error);
     }
